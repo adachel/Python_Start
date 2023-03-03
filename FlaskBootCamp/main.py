fa048d4bbc186750e28d3ce758ea52fec2e42f47
Dmitry <adachel74@gmail.com>
2023-03-03 23:51:41 +0500

    Прост сервер

--- FlaskBootCamp/main.py
+++ FlaskBootCamp/main.py
@@ -1,2 +1,17 @@
-print(2)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return "<h1> Hello World </h1><br><a href='/index'> перейти на вторую страницу</a>"
+
+@app.route('/index/<x>/<y>')
+def index(x, y):
+    return f'Результат: {int(x) + int(y)}'
+
+
+if __name__=='__main__':
+    app.run()
+    
 
