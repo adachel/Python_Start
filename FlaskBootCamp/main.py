7e82b3eef01cc573e953ea69f9b024dbd61405d0
Dmitry <adachel74@gmail.com>
2023-03-11 22:33:26 +0500

    Ok

--- FlaskBootCamp/main.py
+++ FlaskBootCamp/main.py
@@ -1,33 +1,27 @@
 from flask import Flask, render_template
-from random import randint as rd
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def main():
-    name = rd(0, 1)
-    return render_template('base.html', name = name)
+    with open('D:/Works/IT/Python_Start/FlaskBootCamp/file.txt', 'r', encoding='utf-8') as file:
+        resaltData = list()    
+        for line in file.readlines():
+            resaltData.append(tuple(line.split('\n')[0].split(';')))
+    return render_template('base.html', data = resaltData)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
     app.run()
 
-
-
-
-# file = open('D:/Works/IT/Python_Start/FlaskBootCamp/file.txt', 'r', encoding='utf-8')
 # a - режим добавления
 # w - режим записи, очищяет файл
 # r - режим считывания
 
-# list_1 = list()
-# resaltData = list()
-# for line in file.readlines():
-    
-#     resaltData.append(tuple(line.split('\n')[0].split(';')))
 
-# file.close()
-# print(resaltData)
 
 
