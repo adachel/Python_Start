6f856c378ee93a6a44fcf4fe0077910307787b5f
Dmitry <adachel74@gmail.com>
2023-03-11 21:47:41 +0500

    Random yes no

--- FlaskBootCamp/main.py
+++ FlaskBootCamp/main.py
@@ -1,31 +1,33 @@
-# from flask import Flask, render_template
+from flask import Flask, render_template
+from random import randint as rd
+
+app = Flask(__name__)
 
 
-# app = Flask(__name__)
+@app.route('/')
+def main():
+    name = rd(0, 1)
+    return render_template('base.html', name = name)
+
+
+if __name__ == "__main__":
+    app.run()
 
 
 
-# @app.route('/')
-# def main():
-#     return "<h1> Hello World </h1><br><a href='/index'> перейти на вторую страницу</a>"
 
-# @app.route('/index/<x>/<y>')
-# def index(x, y):
-#     return f'Результат: {int(x) + int(y)}'
-
-
-# if __name__=='__main__':
-#     app.run()
-
-# //////////////////////////////////////////////////////////////////////////////////
-# работа с файлом
-#file = open("D:/Works/IT/Python_Start/FlaskBootCamp/file.txt", "r", encoding='utf-8')
-# a - режим добавления. Если а+  и такого файла нет, то он будет создан
-# w - режим на запись(очищает файл)
+# file = open('D:/Works/IT/Python_Start/FlaskBootCamp/file.txt', 'r', encoding='utf-8')
+# a - режим добавления
+# w - режим записи, очищяет файл
 # r - режим считывания
-# list_1 = list() # Создаем лист, это массив с разными типами данных
+
+# list_1 = list()
 # resaltData = list()
-# for line in file.readlines(): # проходим по каждой строке
-#     resaltData.append(tuple(line.split('\n')[0].split(' '))) # делаем кортеж
+# for line in file.readlines():
+    
+#     resaltData.append(tuple(line.split('\n')[0].split(';')))
+
 # file.close()
 # print(resaltData)
+
+
