86cd45990376a8ad97d9c8da1965818399508b15
Dmitry <adachel74@gmail.com>
2023-03-12 22:42:50 +0500

    РегистрАвториз

--- FlaskBootCamp/main.py
+++ FlaskBootCamp/main.py
@@ -1,19 +1,47 @@
 from flask import Flask, render_template
+from LoginForm import Lf
+from AuthForm import AuthF
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hello' # защита от CSRF атак
 
 @app.route('/')
 def main():
-    with open('D:/Works/IT/Python_Start/FlaskBootCamp/file.txt', 'r', encoding='utf-8') as file:
-        resaltData = list()    
-        for line in file.readlines():
-            resaltData.append(tuple(line.split('\n')[0].split(';')))
-    return render_template('base.html', data = resaltData)
+    return render_template('base.html')
+
+@app.route('/register', methods = ['GET', 'POST'])
+def reg():
+    form = Lf()
+    if form.validate_on_submit():
+        if form.password_again.data != form.password.data:
+            return render_template('register.html', title = 'Регистрация', form = form, message = 'Пароли не совподают!')
+        
+        with open('file.txt', 'a', encoding = 'utf - 8') as file:
+            file.write(f"{form.name.data};{form.email.data};{form.password.data}\n")
+        return render_template('register.html', message = 'Регистрация прошла успешно!')
+
+    return render_template('register.html', title = 'Регистрация', form = form)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+@app.route('/log', methods = ['GET', 'POST'])
+def log():
+    form = AuthF()
+    if form.validate_on_submit():
+        with open('file.txt', 'r', encoding = 'utf - 8') as file:
+            data = ' '.join(file.readlines())
+            
+        if form.email.data not in data:
+            return render_template('login.html', form = form, message = 'Вы не зарегистрированы')
+        else:
+            for i in data.split():
+                if form.email.data in i:
+                    if i.split(';')[-1] == form.password.data:
+                        return render_template('login.html', form = form, message = 'Вы успешно авторизовались')        
+    
+    return render_template('login.html', form = form)
 
 if __name__ == "__main__":
     app.run()
